b6f9b6042f9f1aa3110a15822eb5bdb41ab22f5a
saimun <imran.saimun@bdipo.com>
2018-01-01 14:45:52 +0600

    chapter 10

--- app/controllers/concerns/current_cart.rb
+++ app/controllers/concerns/current_cart.rb
@@ -1,12 +1,12 @@
 module CurrentCart
 extend ActiveSupport::Concern
-
+@time = Time.now
 private
  def set_cart
   @cart = Cart.find(session[:cart_id])
-  rescue ActiveRecoed::RecordNotFound
+  rescue ActiveRecord::RecordNotFound
   @cart = Cart.create
    session[:cart_id] = @cart.id
    end 
-    end
+ end
    
--- app/models/product.rb
+++ app/models/product.rb
@@ -14,7 +14,7 @@ def self.latest
 end
 private
 def ensure_not_referenced_by_any_line_item
-if line_item.empty?
+if line_items.empty?
 return true
 else errors.add(:base, 'Line Items present')
 return false
