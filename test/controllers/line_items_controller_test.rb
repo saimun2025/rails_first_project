c7cc16943f14c08998699cb358163a2bc4c3a13d
saimun <imran.saimun@bdipo.com>
2018-01-03 17:44:28 +0600

    12.2

--- test/controllers/line_items_controller_test.rb
+++ test/controllers/line_items_controller_test.rb
@@ -39,6 +39,17 @@ class LineItemsControllerTest < ActionController::TestCase
     patch :update, id: @line_item, line_item: { product_id: @line_item.product_id }
     assert_redirected_to line_item_path(assigns(:line_item))
   end
+  
+  test "should create line_item via ajax" do 
+  assert_difference('LineItem.count') do
+  xhr :post, :create, product_id: products(:ruby).id
+  end 
+  assert_response :success
+  assert_select_jquery :html, '#cart' do
+  assert_select 'tr#current_item td', /Programming Ruby 1.9/
+  end
+  end
+  
 
   test "should destroy line_item" do
     assert_difference('LineItem.count', -1) do
